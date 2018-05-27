6ab78c955a5b9171171aa8429fd9c633d0a65248
getbackwards <tgray@tmmac002606.trademe.local>
2018-05-28 11:56:43 +1200

    section 2/10 Template syntax

--- src/app/products.component.ts
+++ src/app/products.component.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html'
 })
-export class ProductsComponent {}
+export class ProductsComponent {
+  productName = 'A book';
+
+  isDisabled = true;
+  constructor() {
+    setTimeout(() => {
+      // this.productName = 'A tree';
+      this.isDisabled = false;
+    }, 3000);
+  }
+}
