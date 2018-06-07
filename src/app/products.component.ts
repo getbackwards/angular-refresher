3c91ce8b925868193055897ee6e2c0fb6c6dca72
getbackwards <getbackwards@gmail.com>
2018-06-07 19:49:02 +1200

    13 Forms

--- src/app/products.component.ts
+++ src/app/products.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-products',
@@ -6,12 +6,25 @@ import {Component} from '@angular/core';
 })
 export class ProductsComponent {
   productName = 'A book';
-
   isDisabled = true;
+  products = ['A Book', 'A Tree'];
+
   constructor() {
     setTimeout(() => {
       // this.productName = 'A tree';
       this.isDisabled = false;
     }, 3000);
+  }
+
+  onAddProduct(form) {
+    // this.products.push(this.productName);
+    // console.log(form);
+    if (form.valid) {
+      this.products.push(form.value.productName);
+    }
+  }
+
+  onRemoveProduct(productName: string) {
+    this.products = this.products.filter(p => p !== productName);
   }
 }
